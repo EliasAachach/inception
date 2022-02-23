f59424195f06101fca7119a64c988994f43b4848
Elias AACHACH <elaachac@e2r4p18.clusters.42paris.fr>
2022-02-23 15:20:37 +0100

    services working fine. need to check for errors

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,16 +1,16 @@
 if [ ! -d /var/lib/mysql/wordpress ]; then
-	echo "First Run"
+	echo "First run"
 	mysqld&
 	until mysqladmin ping; do
 		sleep 2
 	done
 	mysql -u root -e "SET GLOBAL general_log=1;"
     mysql -u root -e "SET GLOBAL general_log_file='mariadb.log';"
-    mysql -u root -e "CREATE DATABASE db_wordpress;"
-    mysql -u root -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
-    mysql -u root -e "GRANT ALL ON *.* TO 'admin'@'%';"
-    mysql -u root -e "CREATE USER 'wp_user'@'%' IDENTIFIED BY 'wp_user';"
-    mysql -u root -e "GRANT ALL ON db_wordpress.* TO 'wp_user'@'%';"
+    mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+    mysql -u root -e "CREATE USER '${DB_ROOT}'@'%' IDENTIFIED BY '${DB_ROOTPSWRD}';"
+    mysql -u root -e "GRANT ALL ON *.* TO '${DB_ROOT}'@'%';"
+    mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USERPSWRD}';"
+    mysql -u root -e "GRANT ALL ON db_wordpress.* TO '${DB_USER}'@'%';"
     mysql -u root -e "FLUSH PRIVILEGES;"
 	killall mysqld
 	echo "End first run"
